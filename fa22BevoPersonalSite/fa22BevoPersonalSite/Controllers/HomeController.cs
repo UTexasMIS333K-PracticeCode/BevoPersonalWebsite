6d146b9b8f82dd29e4bddaf0528843bc66b7ae74
robertson-utexas <120421665+robertson-utexas@users.noreply.github.com>
2023-09-06 11:42:32 -0500

    commit

---
+++ fa22BevoPersonalSite/fa22BevoPersonalSite/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace fa22BevoPersonalWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+        public HomeController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        //TODO: Create a new action method for the new page
+        
+        
+        public IActionResult Resume()
+        {
+            string path = _environment.WebRootPath + "/files/Bevo_Longhorn_Resume.pdf";
+            var stream = new FileStream(path, FileMode.Open);
+            return File(stream, "application/pdf", "Bevo_Longhorn_Resume.pdf");
+        }
+    }
+}
